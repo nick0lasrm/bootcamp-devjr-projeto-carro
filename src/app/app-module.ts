9b0a245cab67fb4f110dce205b03d26c3553e4c1
Nickolas Machado <nickolasraphael3232@gmail.com>
2025-09-15 12:46:31 -0300

    closes #3 - add footer component

--- src/app/app-module.ts
+++ src/app/app-module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing-module';
 import { App } from './app';
 import { Header } from './components/header/header';
+import { Footer } from './components/footer/footer';
 
 @NgModule({
   declarations: [
     App,
-    Header
+    Header,
+    Footer
   ],
   imports: [
     BrowserModule,
